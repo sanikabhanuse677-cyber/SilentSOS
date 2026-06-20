2af7e7c205fbcff4c5130c646f0c6bb3b1289dfe
Sanika Bhanuse <sanikabhanuse677@gmail.com>
2026-06-20 10:03:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ import { db, ref, set } from "./firebase.js";
 const button = document.getElementById("sosButton");
 
 button.addEventListener("click", () => {
-  console.log("SOS CLICKED");
+ console.log("SOS CLICKED");
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+ navigator.geolocation.getCurrentPosition((position) => {
+ const lat = position.coords.latitude;
+ const lng = position.coords.longitude;
 
-    set(ref(db, "alerts/" + Date.now()), {
-      latitude: lat,
-      longitude: lng
-    });
+ set(ref(db, "alerts/" + Date.now()), {
+ latitude: lat,
+ longitude: lng
+ });
 
-    alert("SOS Sent!");
-  });
+ alert("SOS Sent!");
+ });
 });
